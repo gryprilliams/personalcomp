1288550d96a490831620ea6ee6642cfe5b0e4d3f
gryprilliams <gageryanprince@gmail.com>
2023-01-19 23:42:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# personalcomp
+ Personal Composition for N220
---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+function setup () {
+    createCanvas(windowWidth, windowHeight);
+
+    fill (200, 0, 225);
+    rect (500, 200, 400, 200);
+    circle (830, 255, 75);
+    circle (570, 255, 75);
+    circle (700, 255, 75);
+    circle (830, 345, 75);
+    circle (570, 345, 75);
+    circle (700, 345, 75);
+}
